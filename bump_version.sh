5547da0c7b0c4f67846ab9df80f432b0e85acc41
ShadowM00n <shadowm00n@airmail.cc>
2018-04-07 13:51:11 -0500

    Added support for alpha/beta/rc stages alongside normal bumps

--- bump_version.sh
+++ bump_version.sh
@@ -10,14 +10,17 @@ version=""
 major=0
 minor=0
 patch=0
+stage=""
 
 usage() {
     echo "\
-Usage: $0 type
+Usage: $0 type [stage]
 
 major : x.y.z -> x+1.0.0 ; use for breaking changes
 minor : x.y.z -> x.y+1.0 ; use for new backward compatible features
 patch : x.y.z -> x.y.z+1 ; use for bugfixes or minor updates
+
+stages can be any one of 'alpha', 'beta', or 'rc'
     "
     exit
 }
@@ -38,11 +41,11 @@ write_version() {
             echo "Bumping $file ..."
 
             # This line searches for our $VERSION in funtoo-report and in Report.pm
-            sed -i -e "s/^our \$VERSION = '\([0-9]\+[.]\)\{2\}[0-9].*/our \$VERSION = '$major.$minor.$patch';/" "$path/$file"
+            sed -i -e "s/^our \$VERSION = '\([0-9]\+[.]\)\{2\}[0-9].*/our \$VERSION = '$major.$minor.$patch$stage';/" "$path/$file"
             # This line searches for Version x.y.z found in Report.pm in POD
-            sed -i -e "s/^Version \([0-9]\+[.]\)\{2\}[0-9].*/Version $major.$minor.$patch/" "$path/$file"
+            sed -i -e "s/^Version \([0-9]\+[.]\)\{2\}[0-9].*/Version $major.$minor.$patch$stage/" "$path/$file"
             # This line searches for version number in README.md
-            sed -i -e "s/^# Funtoo-Report - v\([0-9]\+[.]\)\{2\}[0-9].*!/# Funtoo-Report - v$major.$minor.$patch !/" "$path/$file"
+            sed -i -e "s/^# Funtoo-Report - v\([0-9]\+[.]\)\{2\}[0-9].*!/# Funtoo-Report - v$major.$minor.$patch$stage !/" "$path/$file"
 
         done
     }
@@ -73,7 +76,24 @@ write_version() {
             ;;
     esac
 
-    echo "Funtoo::Report updating $version -> $major.$minor.$patch"
+    case "$2" in
+        alpha)
+            stage='-alpha'
+            ;;
+        beta)
+            stage='-beta'
+            ;;
+        rc)
+            stage='-rc'
+            ;;
+        "")
+            ;;
+        *)
+            usage
+            ;;
+    esac
+
+    echo "Funtoo::Report updating $version -> $major.$minor.$patch$stage"
     echo "**************************************"
 
     write_version
